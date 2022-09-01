c35be2c1d6eeee2da9391ed4a1edaac7c21587bf
Steboss89 <stbo@trustpilot.com>
2022-09-01 16:18:11 +0100

    custom nima

---
+++ inference_model.py
@@ -0,0 +1,117 @@
+from pathlib import Path
+
+import torch
+from PIL.Image import Image
+from torchvision.datasets.folder import default_loader
+
+
+
+use_cuda = torch.cuda.is_available()
+device = torch.device("cuda" if use_cuda else "cpu")
+
+import torch.nn as nn
+import torchvision as tv
+
+
+MODELS = {
+    "resnet18": (tv.models.resnet18, 512),
+    "resnet34": (tv.models.resnet34, 512),
+    "resnet50": (tv.models.resnet50, 2048),
+    "resnet101": (tv.models.resnet101, 2048),
+    "resnet152": (tv.models.resnet152, 2048),
+}
+
+
+class NIMA(nn.Module):
+    def __init__(self, base_model: nn.Module, input_features: int, drop_out: float):
+        super(NIMA, self).__init__()
+        self.base_model = base_model
+
+        self.head = nn.Sequential(
+            nn.ReLU(inplace=True), nn.Dropout(p=drop_out), nn.Linear(input_features, 10), nn.Softmax(dim=1)
+        )
+
+    def forward(self, x):
+        x = self.base_model(x)
+        x = x.view(x.size(0), -1)
+        x = self.head(x)
+        return x
+
+
+def create_model(model_type: str, drop_out: float) -> NIMA:
+    create_function, input_features = MODELS[model_type]
+    base_model = create_function(pretrained=True)
+    base_model = nn.Sequential(*list(base_model.children())[:-1])
+    return NIMA(base_model=base_model, input_features=input_features, drop_out=drop_out)
+
+
+
+def format_output(mean_score, std_score, prob):
+    return {"mean_score": float(mean_score), "std_score": float(std_score), "scores": [float(x) for x in prob]}
+
+def get_mean_score(score):
+    buckets = np.arange(1, 11)
+    mu = (buckets * score).sum()
+    return mu
+
+
+def get_std_score(scores):
+    si = np.arange(1, 11)
+    mean = get_mean_score(scores)
+    std = np.sqrt(np.sum(((si - mean) ** 2) * scores))
+    return std
+
+
+class Transform:
+    def __init__(self):
+        normalize = transforms.Normalize(mean=IMAGE_NET_MEAN, std=IMAGE_NET_STD)
+
+        self._train_transform = transforms.Compose(
+            [
+                transforms.Resize((256, 256)),
+                transforms.RandomHorizontalFlip(),
+                transforms.RandomCrop((224, 224)),
+                transforms.ToTensor(),
+                normalize,
+            ]
+        )
+
+        self._val_transform = transforms.Compose([transforms.Resize((224, 224)), transforms.ToTensor(), normalize])
+
+    @property
+    def train_transform(self):
+        return self._train_transform
+
+    @property
+    def val_transform(self):
+        return self._val_transform
+
+
+class InferenceModel:
+    def __init__(self, path_to_model_state: Path):
+        self.transform = Transform().val_transform
+        model_state = torch.load(path_to_model_state, map_location=lambda storage, loc: storage)
+        self.model = create_model(model_type=model_state["model_type"], drop_out=0)
+        self.model.load_state_dict(model_state["state_dict"])
+        self.model = self.model.to(device)
+        self.model.eval()
+
+    def predict_from_file(self, image_path: Path):
+        image = default_loader(image_path)
+        return self.predict(image)
+
+    def predict_from_pil_image(self, image: Image):
+        image = image.convert("RGB")
+        return self.predict(image)
+
+    @torch.no_grad()
+    def predict(self, image):
+        image = self.transform(image)
+        image = image.unsqueeze_(0)
+        image = image.to(device)
+        prob = self.model(image).data.cpu().numpy()[0]
+
+        mean_score = get_mean_score(prob)
+        std_score = get_std_score(prob)
+
+        return format_output(mean_score, std_score, prob)
